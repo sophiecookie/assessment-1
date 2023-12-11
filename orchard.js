af52f2a697a4d7c7f18bd6e5cb5fe4e53e32f8de
Di Yang <104461485+sophiecookie@users.noreply.github.com>
2023-12-10 23:10:53 -0800

    finished assessment 1

---
+++ orchard.js
@@ -0,0 +1,221 @@
+///////////////////
+// Apple Orchard //
+///////////////////
+
+/*
+    The data below has been collected over the last week at an apple orchard. 
+    You will be using your coding skills to gain insights into its daily 
+    operations.
+
+    Below are three arrays each containing 7 numbers. You will use them and the 
+    price variables to complete the problems in this file.
+
+    The indexes of the arrays correspond with a day of the week.
+    The first array is named fujiAcres. Notice that the first item in this array 
+    is 2, meaning that 2 acres of Fuji apples were picked on Monday of this week.
+*/
+
+const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
+const galaAcres = [5, 2, 4, 3, 6, 2, 4]
+const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
+
+
+
+
+
+// PROBLEM 1
+
+/*
+    Using a for loop, calculate the total number of acres picked for the entire 
+    week.
+
+    Save the number to a variable called `totalAcres`.
+    Log `totalAcres` to the console.
+*/
+
+// CODE HERE
+
+//create variable totalAcres to save the number
+let totalAcres = 0;
+
+//create a loop to add acres picked in different types of apples
+for(let i = 0; i < fujiAcres.length; i++){
+    totalAcres += fujiAcres[i] + galaAcres[i] + pinkAcres[i];
+}
+
+//console log the result
+console.log(totalAcres);
+
+
+
+// PROBLEM 2
+
+/*
+    Using `totalAcres`, calculate the average number of acres picked per day. 
+
+    (Remember that the average is the total divided by the number of days)
+
+    Save the answer to a variable called 
+    `averageDailyAcres`, and log `averageDailyAcres` to the console.
+*/
+
+// CODE HERE
+
+//calculate average daily acres
+const averageDailyAcres = totalAcres / fujiAcres.length;
+
+//console log the result
+console.log(averageDailyAcres);
+
+
+
+
+// PROBLEM 3
+
+/*
+    We have provided 2 variables below. 
+    
+    The `acresLeft` variable is the number of acres that still have apples left. 
+    
+    The `days` variable represents how many more days of work are left. It's 
+    initialized at 0 because we're going to be using it as a counter.
+    
+    Write a while loop that will continue to run while `acresLeft` is above 0. 
+    
+    On each iteration of the loop:
+    - add 1 to the `days` variable
+    - subtract your daily average from the number of acres left
+
+    Outside the loop, log `days` to the console.
+
+    Note: This is not the most efficient way to calculate this number. You might
+    think about other ways you could do it more mathematically.
+*/
+
+let acresLeft = 174 
+let days = 0
+
+// CODE HERE
+
+//create a while loop to calculate days
+while(acresLeft > 0){
+    days++;
+    acresLeft -= averageDailyAcres;
+}
+
+//console log the result
+console.log(days);
+
+
+// PROBLEM 4
+
+/*
+    Your next task is to create 3 arrays that list the daily amount of apples 
+    picked, in tons, for each variety. 
+    
+    Each acre yields 6.5 tons of apples.
+
+    Use the variables below to store your new arrays. Make sure that you
+    don't modify the original arrays on lines 36 - 38.
+
+    Log each of your arrays to the console.
+
+    (Hint: loop over the original arrays, calculate the tons for that day, then
+    add that number into the new list)
+*/
+
+// CODE HERE
+
+//create three arrays to log the result
+let fujiTons = [];
+let galaTons = [];
+let pinkTons = [];
+
+//create a for loop to push tons of apple per day to the array of different apple types
+for(let i = 0; i < fujiAcres.length; i++){
+    fujiTons.push(fujiAcres[i] * 6.5);
+    galaTons.push(galaAcres[i] * 6.5);
+    pinkTons.push(pinkAcres[i] * 6.5);
+}
+
+//console log the result
+console.log(fujiTons);
+console.log(galaTons);
+console.log(pinkTons);
+
+// PROBLEM 5
+
+/*
+    Next, calculate the TOTAL number of pounds picked per variety.
+
+    You'll need to add up the tons per each variety and convert the number 
+    into pounds -- store that number in the variables given below. 
+
+    Log each of the values to the console.
+
+    Hint: there are 2000 pounds in a ton.
+*/
+
+// CODE HERE 
+
+//create three variables to store results
+let fujiPounds = 0;
+let galaPounds = 0;
+let pinkPounds = 0;
+
+//create a for loop to calculate total pounds of apples per type
+for(let i = 0; i < fujiTons.length; i++){
+    fujiPounds += fujiTons[i] * 2000;
+    galaPounds += galaTons[i] * 2000;
+    pinkPounds += pinkTons[i] * 2000;
+}
+
+//console log the result
+console.log(fujiPounds);
+console.log(galaPounds);
+console.log(pinkPounds);
+
+
+// PROBLEM 6
+
+/*
+    Now that you know the total pounds per variety, use the prices below to 
+    figure out how much you'll make from selling each type of apple. 
+
+    The prices are per pound and are written in cents. 
+
+    Log each of the profits to the console. 
+*/
+const fujiPrice = .89 
+const galaPrice = .64
+const pinkPrice = .55
+
+// CODE HERE
+
+//store the total profit after calculating using: pounds * price
+let fujiProfit = fujiPounds * fujiPrice;
+let galaProfit = galaPounds * galaPrice;
+let pinkProfit = pinkPounds * pinkPrice;
+
+//console log the result
+console.log(fujiProfit);
+console.log(galaProfit);
+console.log(pinkProfit);
+
+
+// PROBLEM 7
+
+/*
+    Add up all your profits and save  the number to a variable called 
+    `totalProfit`.
+
+    Log `totalProfit` to the console.
+*/
+
+// CODE HERE
+
+//calculate the total profits by adding up profits of three apple types
+let totalProfit = fujiProfit + galaProfit + pinkProfit;
+
+//console log the result
+console.log(totalProfit);
